7ffe03caa917b94ddc05b6cb3bd978a4b1bd2ef9
tomeklarysz <ttomek.larysz@gmail.com>
2025-10-05 01:42:41 +0200

    add controller animation

--- Assets/Scripts/BugSpawner.cs
+++ Assets/Scripts/BugSpawner.cs
@@ -2,40 +2,20 @@ using UnityEngine;
 
 public class BugSpawner : MonoBehaviour
 {
-    private GameObject spawner;
-    private GameObject bugPrefab;
+    [SerializeField] private GameObject spawner;
+    [SerializeField] private GameObject bugPrefab;
     private GameObject bug;
 
     void Start()
     {
-        spawner = GameObject.Find("Spawner");
-        bugPrefab = Resources.Load<GameObject>("Prefabs/Bug"); // Added missing semicolon
-
-        if (spawner == null)
-        {
-            Debug.LogError("Spawner GameObject not found in scene!");
-        }
-        else
-        {
-            Debug.Log("Spawner found at position: " + spawner.transform.position);
-        }
-
-        if (bugPrefab == null)
-        {
-            Debug.LogError("Bug prefab not found in Resources folder!");
-        }
-        else
-        {
-            Debug.Log("Bug prefab loaded successfully: " + bugPrefab.name);
-        }
-
-
         // Instantiate the prefab to create a game object
         if (bugPrefab != null)
         {
-            bug = Instantiate(bugPrefab);
-            // Optional: Set position relative to spawner
-            bug.transform.position = Vector3.zero;
+            bug = Instantiate(bugPrefab, spawner.transform);
+            bug.transform.position = spawner.transform.position;
+            //bug.transform.position = new Vector2(0, 0);
+            Debug.Log("spawner transform position: " + spawner.transform.position);
+            Debug.Log("Bug spawned at spawner position." + bug.transform.position);
         }
         else
         {
